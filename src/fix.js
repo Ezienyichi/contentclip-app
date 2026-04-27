87c23ea756fa417b121653e4c664821a230df91c
Ezienyichi <lindasdoc@gmail.com>
2026-04-27 16:13:00 +0100

    fix import page

--- src/fix.js
+++ src/fix.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-const lines = fs.readFileSync('src/app/import/page.tsx', 'utf8').split('\n');
-lines.splice(242, 8);
-fs.writeFileSync('src/app/import/page.tsx', lines. join('\n'));
-console.log('Done! Removed broken lines.');
-console.log('New line 242:', lines[242]);
-console.log('New line 243:', lines[243]);
+let c = fs.readFileSync('src/app/import/page.tsx', 'utf8'); 
+c = c.replace( 
+    'data: { user }, \n       } = await supabase,auth,getuser();',
+    'data: { session },\n       } = await supabase.auth.getSession();\n      const user = session?.user;'
+);    
+fs.writerFileSync('src/app/import/page.tsx', c);
+console.log('done');
